3adeecceb25a0d1497c2067877abb5c60ed167ca
Josh <joshua.lopes@ocsbstudent.com>
2013-12-13 09:11:02 -0500

    Added

--- JawApp Project/gen/com/example/jawappproject/R.java
+++ JawApp Project/gen/com/example/jawappproject/R.java
@@ -46,13 +46,19 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int textView1=0x7f090004;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int activity_splash_screen=0x7f030001;
-        public static final int activity_targeting=0x7f030002;
+        public static final int activity_basic_income=0x7f030000;
+        public static final int activity_commission=0x7f030001;
+        public static final int activity_main=0x7f030002;
+        public static final int activity_spending=0x7f030003;
+        public static final int activity_splash_screen=0x7f030004;
+        public static final int activity_targeting=0x7f030005;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
-        public static final int targeting=0x7f080001;
+        public static final int basic_income=0x7f080000;
+        public static final int commission=0x7f080001;
+        public static final int main=0x7f080002;
+        public static final int spending=0x7f080003;
+        public static final int targeting=0x7f080004;
     }
     public static final class string {
         public static final int MMm=0x7f060008;
@@ -67,6 +73,9 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int dummy_button=0x7f06000b;
         public static final int dummy_content=0x7f06000c;
         public static final int hello_world=0x7f060002;
+        public static final int title_activity_basic_income=0x7f060010;
+        public static final int title_activity_commission=0x7f06000e;
+        public static final int title_activity_spending=0x7f06000f;
         public static final int title_activity_splash_screen=0x7f06000a;
         public static final int title_activity_targeting=0x7f06000d;
     }
