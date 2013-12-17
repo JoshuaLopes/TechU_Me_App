ac3732dcab7f671ad57ff451cd40c6e62ef8878f
Alex.M <alex.mushyirahamwe@ocsbstudent.ca>
2013-12-17 08:43:09 -0500

    committed

--- JawApp Project/gen/com/example/jawappproject/R.java
+++ JawApp Project/gen/com/example/jawappproject/R.java
@@ -46,19 +46,13 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int textView1=0x7f090004;
     }
     public static final class layout {
-        public static final int activity_basic_income=0x7f030000;
-        public static final int activity_commission=0x7f030001;
-        public static final int activity_main=0x7f030002;
-        public static final int activity_spending=0x7f030003;
-        public static final int activity_splash_screen=0x7f030004;
-        public static final int activity_targeting=0x7f030005;
+        public static final int activity_main=0x7f030000;
+        public static final int activity_splash_screen=0x7f030001;
+        public static final int activity_targeting=0x7f030002;
     }
     public static final class menu {
-        public static final int basic_income=0x7f080000;
-        public static final int commission=0x7f080001;
-        public static final int main=0x7f080002;
-        public static final int spending=0x7f080003;
-        public static final int targeting=0x7f080004;
+        public static final int main=0x7f080000;
+        public static final int targeting=0x7f080001;
     }
     public static final class string {
         public static final int MMm=0x7f060008;
@@ -83,18 +77,15 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
-
     
 
             Theme customizations available in newer API levels can go in
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
-
         
 
         Base application theme for API 11+. This theme completely replaces
         AppBaseTheme from res/values/styles.xml on API 11+ devices.
-
     
  API 11 theme customizations can go here. 
 
