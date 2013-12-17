95799f892714f50062095ccacba0d627f93e8159
Alex.M <alex.mushyirahamwe@ocsbstudent.ca>
2013-12-17 09:08:40 -0500

    i've made changes

--- JawApp Project/gen/com/example/jawappproject/R.java
+++ JawApp Project/gen/com/example/jawappproject/R.java
@@ -35,12 +35,14 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090008;
-        public static final int button1=0x7f090000;
+        public static final int action_settings=0x7f09000a;
+        public static final int button1=0x7f090001;
         public static final int button2=0x7f090003;
-        public static final int button3=0x7f090001;
+        public static final int button3=0x7f090000;
         public static final int button4=0x7f090002;
         public static final int dummy_button=0x7f090007;
+        public static final int editText1=0x7f090008;
+        public static final int editText2=0x7f090009;
         public static final int fullscreen_content=0x7f090005;
         public static final int fullscreen_content_controls=0x7f090006;
         public static final int textView1=0x7f090004;
